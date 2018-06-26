6bf68d5d13188420f68dc484e7ede5cb14a36bfa
Michael Liendo <model3volution@gmail.com>
2018-06-25 22:25:57 -0500

    feat: add clapback functionality

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const slackv1Router = require('./routes/slackv1-router')
 const {PORT} = process.env
 
+app.use(bodyParser.urlencoded({extended:false}))
 app.use('/slack/v1',slackv1Router)
 
 app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
--- routes/slackv1-router.js
+++ routes/slackv1-router.js
@@ -2,7 +2,16 @@ const express = require('express')
 const slackv1Router = express.Router()
 
 slackv1Router.post('/sassy', (req,res) => {
-  res.send('hello')
-})
+  const {text} = req.body
+  // 1. trim outside whitespace
+  // 2. capitalize
+  // 3. split by internal whitespace
+  // 4. append 👏🏾 to the end of each work
+  // 5. join back together
+
+  const  clapbackText = text.trim().toUpperCase().split(' ').map(word => `${word} :clap::skin-tone-5:`).join(' ')
+  console.log(clapbackText)
+  res.status(200).send(clapbackText)
+}) 
 
 module.exports = slackv1Router
