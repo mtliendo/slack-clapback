327b1ab156118566e8fdaaca02b9e2dd9b185961
Michael Liendo <model3volution@gmail.com>
2018-07-02 23:09:57 -0500

    feat: test: comput hmac value

--- routes/slackv1-router.js
+++ routes/slackv1-router.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const slackv1Router = express.Router()
 const {getSlackToken} = require('../services/slack-oauth')
-const {clapback} = require('../services/slack-transforms')
+const {clapback,getFormData} = require('../services/slack-transforms')
+const crypto = require('crypto')
 
 const {CLIENT_ID, CLIENT_SECRET} = process.env
 
 slackv1Router.post('/sassy', (req,res) => {
-
-  console.log(req.get('X-Slack-Request-Timestamp'))
-  console.log(req.body)
+  
   const {text} = req.body
+  const formData = getFormData(req.body)
+  const timestamp = req.get('X-Slack-Request-Timestamp')
+  const baseString = `v0:${timestamp}:${formData}`
+  const hmac = crypto.createHmac('sha256', baseString)
   const clapbackText = clapback(text)
+
+  console.log('the hmac value is:', hmac)
+
   res.status(200).json({
     response_type: "in_channel",
     text: clapbackText
