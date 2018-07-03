936c37e003ef66122af0900f48a04149a55771e5
Michael Liendo <model3volution@gmail.com>
2018-07-02 22:21:36 -0500

    test: debug header

--- routes/slackv1-router.js
+++ routes/slackv1-router.js
@@ -6,6 +6,7 @@ const {clapback} = require('../services/slack-transforms')
 const {CLIENT_ID, CLIENT_SECRET} = process.env
 
 slackv1Router.post('/sassy', (req,res) => {
+  console.log(req)
   const {text} = req.body
   const clapbackText = clapback(text)
   res.status(200).json({
